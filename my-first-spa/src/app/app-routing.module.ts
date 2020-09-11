2af0f637d4685cc326945566159c1abcfa891321
= <=>
2020-09-11 15:19:23 -0400

    changes to lazy loading

--- my-first-spa/src/app/app-routing.module.ts
+++ my-first-spa/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Router, RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 const routes: Routes = [
+    {path: 'employees', loadChildren: () => import('./employee/employee.module').then(e => e.EmployeeModule)},
     {path: 'home', component: HomeComponent},
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: '**', component: PageNotFoundComponent}
