4ee016e41c5b9fe75f47aea04e773fc48c1a74a9
= <=>
2020-09-11 15:00:16 -0400

    changes to routes

---
+++ my-first-spa/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Router, RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: '**', component: PageNotFoundComponent}
+]
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+}
